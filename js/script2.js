805f946e260b1c9206ce1f64a9c8965cbb34c92d
Dima <dmitriidemidov93@gmail.com>
2020-05-28 23:48:44 +0300

    First commit

---
+++ js/script2.js
@@ -0,0 +1,22 @@
+let  money = prompt("Ваш бюджет на месяц?"),
+    date  = prompt ("Введите дату в формате YYYY-MM-DD");
+
+
+appDate = {
+    budget: money,
+    timeData: date,
+    expenses: {},
+    optionalExpenses: {},
+    income: null,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a2 = prompt("Во сколько обойдется?"),
+    a3 = prompt("Введите обязательную статью расходов в этом месяце"),
+    a4 = prompt("Во сколько обойдется?");
+
+appDate.expenses.a1 = a2;
+appDate.expenses.a3 = a4;
+
+alert (appDate.budget / 30);
